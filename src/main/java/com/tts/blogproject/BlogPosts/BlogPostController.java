bc3d912a500003b7946231788e89ce8ff2ac34a5
Jimmy Dang <jdang@empoweredbenefits.com>
2019-02-19 11:25:39 -0500

    added views

--- src/main/java/com/tts/blogproject/BlogPosts/BlogPostController.java
+++ src/main/java/com/tts/blogproject/BlogPosts/BlogPostController.java
@@ -6,26 +6,32 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class BlogPostController {
 
     @Autowired
     private BlogPostRepository blogPostRepository;
+    private static List<BlogPost> posts = new ArrayList<>();
 
-    @GetMapping(value="/")
-    public String index(BlogPost blogPost) {
-        return "blogpost/index";    }
+    @GetMapping(value="/blog_posts/new")
+    public String index(BlogPost blogPost, Model model) {
+        model.addAttribute("posts", posts);
+        return "blogposts/new";    }
 
     //	private BlogPost blogPost;
-    @PostMapping(value = "/")
+    @PostMapping(value = "/blog_posts/new")
     public String addNewBlogPost(BlogPost blogPost, Model model) {
         // Shortcut (remains to be seen if works):
         blogPostRepository.save(blogPost);
+        posts.add(blogPost);
         //Longer version:
         //blogPostRepository.save(new BlogPost(blogPost.getTitle(), blogPost.getAuthor(), blogPost.getBlogEntry()));
         model.addAttribute("title", blogPost.getTitle());
         model.addAttribute("author", blogPost.getAuthor());
         model.addAttribute("blogEntry", blogPost.getBlogEntry());
-        return "blogpost/result";
+        return "blogposts/result";
     }
 }
--- src/main/java/com/tts/blogproject/BlogPosts/BlogPostRepository.java
+++ src/main/java/com/tts/blogproject/BlogPosts/BlogPostRepository.java
@@ -3,4 +3,5 @@ package com.tts.blogproject.BlogPosts;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
+
 }
